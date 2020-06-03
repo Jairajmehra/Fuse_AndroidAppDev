56bd1ff9ab20a65a8ec64642bfcec0498a91b088
Jairajmehra <jairaj_mehra@yahoo.com>
2020-06-03 12:48:29 +1000

    Navigation View Added

--- app/src/main/java/com/example/fuse/MainActivity.java
+++ app/src/main/java/com/example/fuse/MainActivity.java
@@ -9,12 +9,14 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     EditText email_edittext, password_edittext;
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final FirebaseAuth fAuth = FirebaseAuth.getInstance();
+
         if(fAuth.getCurrentUser() != null)
         {
             startActivity(new Intent(MainActivity.this, browse_activity.class));
--- app/src/main/java/com/example/fuse/browse_activity.java
+++ app/src/main/java/com/example/fuse/browse_activity.java
@@ -1,30 +1,75 @@
 package com.example.fuse;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class browse_activity extends AppCompatActivity {
+public class browse_activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     Button logout;
+    Toolbar toolbar;
+    DrawerLayout drawer;
+    ActionBarDrawerToggle toogle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse_activity);
-        logout = (Button)findViewById(R.id.logout_button);
+        final FirebaseAuth fAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = fAuth.getCurrentUser();
 
-        
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(browse_activity.this, MainActivity.class));
-                finish();
-            }
-        });
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        View headerView = navigationView.getHeaderView(0);
+        TextView drawerUsername = (TextView) headerView.findViewById(R.id.navigation_view_textview_username);
+        drawerUsername.setText(user.getEmail().toString());
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        toogle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open, R.string.close);
+        drawer.addDrawerListener(toogle);
+        toogle.setDrawerIndicatorEnabled(true);
+        toogle.syncState();
+//
+//        logout = (Button)findViewById(R.id.logout_button);
+//
+//
+//        logout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                FirebaseAuth.getInstance().signOut();
+//                startActivity(new Intent(browse_activity.this, MainActivity.class));
+//                finish();
+//            }
+//        });
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.post_activity)
+        {
+            System.out.println("Post activity");
+        }
+        else if (item.getItemId() == R.id.profile)
+        {
+            System.out.println("profile");
+        }
+        else if (item.getItemId() == R.id.browse_activity)
+        {
+            System.out.println("Browse activity");
+        }
+        return  true;
+    }
+
 }
